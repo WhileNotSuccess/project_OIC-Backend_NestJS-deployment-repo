7dbdb5fbc1d5a49855cb6a4b8ad2076f147b7739
MunSeongyun <borygashill608@gmail.com>
2025-04-07 01:32:06 +0900

    update main.yml

--- api/src/media/infra/local-media.service.spec.ts
+++ api/src/media/infra/local-media.service.spec.ts
@@ -42,7 +42,6 @@ describe("LocalMediaService", () => {
     service = module.get<LocalMediaService>(LocalMediaService);
   });
   beforeAll(async () => {
-    await fs.promises.mkdir("/files");
     const badFilePath = path.resolve(
       __dirname,
       "../../../test/__fixtures__/attachment/bad",
